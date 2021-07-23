9e19d59e208a73ed6089db00e102da5c80bbdc44
xh116 <37950502+xh116@users.noreply.github.com>
2021-07-24 01:09:18 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,9 @@
 
 set -e
 
-set_clash_iptables(){
-      iptables -t nat -A PREROUTING -p tcp -j REDIRECT --to-ports 1080
-      iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 1080
-      }
+iptables -t nat -A PREROUTING -p tcp -j REDIRECT --to-ports 1080
 
-set_clash_iptables
+iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 1080
 
 sysctl -w net/ipv4/ip_forward=1
 
