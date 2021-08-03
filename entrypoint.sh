58d618b66d039d8eb79f000a517993b5a4178114
xh116 <37950502+xh116@users.noreply.github.com>
2021-08-04 03:28:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 
 iptables -t nat -A PREROUTING -p tcp -j REDIRECT --to-ports 1080
 
+iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 1080
+
+
 sysctl -w net/ipv4/ip_forward=1
 
 ip addr
